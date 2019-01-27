1fb0a417c2f11907cc3a991e495cbf2cc8614884
Victor Rodrigues <victorc.rodrigues@gmail.com>
2019-01-27 01:10:14 +0100

    v0.1.10

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ Add `deli` to your deps:
 def deps do
   [
     # ...
-    {:deli, "~> 0.1.9", runtime: false}
+    {:deli, "~> 0.1.10", runtime: false}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Deli.MixProject do
   def project do
     [
       app: :deli,
-      version: "0.1.9",
+      version: "0.1.10",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       application: application(),
