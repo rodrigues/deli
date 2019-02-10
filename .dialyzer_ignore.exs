71d2150de00de4a63ab89bc99f1a96b8ad5e69ad
Victor Rodrigues <victorc.rodrigues@gmail.com>
2019-02-10 15:03:50 +0100

    Ignore dialyzer bugs

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,1 +1,9 @@
-[]
+[
+  # file errors are related to this elixir issue:
+  # https://github.com/elixir-lang/elixir/issues/8631
+  # remove the ignores below when issue is fixed
+  {"lib/deli/beam_versions/file.ex", :no_return, 11},
+  {"lib/deli/beam_versions/updater.ex", :no_return, 55},
+  {"lib/deli/beam_versions/updater.ex", :no_return, 58},
+  {"lib/deli/beam_versions/updater.ex", :no_return, 74}
+]
