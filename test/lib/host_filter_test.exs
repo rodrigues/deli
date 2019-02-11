c07fa60394c82fc87e428e16de6556ea6b209d92
Victor Rodrigues <victorc.rodrigues@gmail.com>
2019-02-11 21:16:02 +0100

    Make test more asserting

--- test/lib/host_filter_test.exs
+++ test/lib/host_filter_test.exs
@@ -10,16 +10,17 @@ defmodule Deli.HostFilterTest do
                 h1 <- ?a..?k |> string() |> except(empty?),
                 h2 <- ?l..?z |> string() |> except(empty?),
                 term_size <- integer(1..5) do
-        put_config(:hosts, [{e, [h1, h2]}])
+        h3 = h1 <> h2
+        put_config(:hosts, [{e, [h1, h2, h3]}])
 
         {filter, _} = h1 |> String.split_at(term_size)
 
         output =
           capture_io(fn ->
-            {:ok, [^h1]} = e |> HostFilter.hosts(["-h", filter])
+            {:ok, [^h1, ^h3]} = e |> HostFilter.hosts(["-h", filter])
           end)
 
-        assert output == "# hosts\n## #{h1}\n"
+        assert output == "# hosts\n## #{h1}\n## #{h3}\n"
       end
     end
   end
