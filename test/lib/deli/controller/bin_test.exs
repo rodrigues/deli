31a16e1531ff848fe4dd8f538c3415b1c27d9c96
Victor Rodrigues <victorc.rodrigues@gmail.com>
2019-02-13 02:09:00 +0100

    Bin.start_host/2

---
+++ test/lib/deli/controller/bin_test.exs
@@ -0,0 +1,56 @@
+defmodule Deli.Controller.BinTest do
+  use DeliCase
+  alias Deli.Controller.Bin
+
+  setup do
+    put_config(:__system__, SystemStub)
+  end
+
+  describe "start_host/2" do
+    property "starts host" do
+      check all app_user <- atom() |> except(&is_nil/1),
+                bin_path <- non_empty_string(),
+                env <- atom() |> except(&is_nil/1),
+                host <- non_empty_string() do
+        put_config(:app_user, app_user)
+        put_config(:bin_path, bin_path)
+        stub_cmd({"", 0})
+        id = env |> Config.host_id(host)
+
+        :ok = env |> Bin.start_host(host)
+
+        assert_receive {
+          :__system__,
+          :cmd,
+          "ssh",
+          [^id, ^bin_path, "start"],
+          _
+        }
+      end
+    end
+
+    property "pings host and errors with not a pong" do
+      check all app_user <- atom() |> except(&is_nil/1),
+                bin_path <- non_empty_string(),
+                env <- atom() |> except(&is_nil/1),
+                host <- non_empty_string(),
+                response <- non_empty_string(),
+                response != "pong" do
+        put_config(:app_user, app_user)
+        put_config(:bin_path, bin_path)
+        stub_cmd({response, 0})
+        id = env |> Config.host_id(host)
+
+        :ok = env |> Bin.start_host(host)
+
+        assert_receive {
+          :__system__,
+          :cmd,
+          "ssh",
+          [^id, ^bin_path, "start"],
+          _
+        }
+      end
+    end
+  end
+end
