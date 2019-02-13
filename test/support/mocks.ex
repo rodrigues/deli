9d235a5ca03499e4fbb06490c98bd4dc626c8d9c
Victor Rodrigues <victorc.rodrigues@gmail.com>
2019-02-13 18:43:50 +0100

    Add mocks support file

---
+++ test/support/mocks.ex
@@ -0,0 +1,4 @@
+Mox.defmock(ControllerMock, for: Deli.Controller)
+Mox.defmock(HostProviderMock, for: Deli.HostProvider)
+Mox.defmock(ReleaseMock, for: Deli.Release)
+Mox.defmock(VersioningMock, for: Deli.Versioning)
