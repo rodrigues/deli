562a96eb1aced85372dce31c5e9c1da9cb212de9
Victor Rodrigues <victorc.rodrigues@gmail.com>
2019-02-13 02:32:28 +0100

    Adds stop and restart tests

--- test/lib/deli/controller/bin_test.exs
+++ test/lib/deli/controller/bin_test.exs
@@ -58,4 +58,110 @@ defmodule Deli.Controller.BinTest do
       end
     end
   end
+
+  describe "stop_host/2" do
+    property "stops host" do
+      check all app_user <- atom() |> except(&is_nil/1),
+                bin_path <- non_empty_string(),
+                env <- atom() |> except(&is_nil/1),
+                host <- non_empty_string() do
+        put_config(:app_user, app_user)
+        put_config(:bin_path, bin_path)
+        stub_cmd({"", 0})
+        id = env |> Config.host_id(host)
+
+        :ok = env |> Bin.stop_host(host)
+
+        assert_receive {
+          :__system__,
+          :cmd,
+          "ssh",
+          [^id, ^bin_path, "stop"],
+          _
+        }
+      end
+    end
+
+    property "fails to stop host if signal not expected" do
+      check all app_user <- atom() |> except(&is_nil/1),
+                bin_path <- non_empty_string(),
+                env <- atom() |> except(&is_nil/1),
+                host <- non_empty_string(),
+                signal <- 1..500 |> integer() do
+        put_config(:app_user, app_user)
+        put_config(:bin_path, bin_path)
+        stub_cmd({"", signal})
+        id = env |> Config.host_id(host)
+
+        call = fn ->
+          capture_io(fn ->
+            env |> Bin.stop_host(host)
+          end)
+        end
+
+        assert catch_exit(call.()) == {:shutdown, signal}
+
+        assert_receive {
+          :__system__,
+          :cmd,
+          "ssh",
+          [^id, ^bin_path, "stop"],
+          _
+        }
+      end
+    end
+  end
+
+  describe "restart_host/2" do
+    property "restarts host" do
+      check all app_user <- atom() |> except(&is_nil/1),
+                bin_path <- non_empty_string(),
+                env <- atom() |> except(&is_nil/1),
+                host <- non_empty_string() do
+        put_config(:app_user, app_user)
+        put_config(:bin_path, bin_path)
+        stub_cmd({"", 0})
+        id = env |> Config.host_id(host)
+
+        :ok = env |> Bin.restart_host(host)
+
+        assert_receive {
+          :__system__,
+          :cmd,
+          "ssh",
+          [^id, ^bin_path, "restart"],
+          _
+        }
+      end
+    end
+
+    property "fails to restart host if signal not expected" do
+      check all app_user <- atom() |> except(&is_nil/1),
+                bin_path <- non_empty_string(),
+                env <- atom() |> except(&is_nil/1),
+                host <- non_empty_string(),
+                signal <- 1..500 |> integer() do
+        put_config(:app_user, app_user)
+        put_config(:bin_path, bin_path)
+        stub_cmd({"", signal})
+        id = env |> Config.host_id(host)
+
+        call = fn ->
+          capture_io(fn ->
+            env |> Bin.restart_host(host)
+          end)
+        end
+
+        assert catch_exit(call.()) == {:shutdown, signal}
+
+        assert_receive {
+          :__system__,
+          :cmd,
+          "ssh",
+          [^id, ^bin_path, "restart"],
+          _
+        }
+      end
+    end
+  end
 end
