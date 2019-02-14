04938fbf817610dfe40bfdae4abb923e46b68642
Victor Rodrigues <victorc.rodrigues@gmail.com>
2019-02-14 23:58:00 +0100

    Add guards for Deli.Deploy

--- lib/deli/deploy.ex
+++ lib/deli/deploy.ex
@@ -1,16 +1,17 @@
 defmodule Deli.Deploy do
-  import Deli.Shell
+  import Deli, only: [is_env: 1, is_host: 1]
+  import Deli.Shell, only: [edeliver: 2]
   alias Deli.{Check, Config}
 
   @moduledoc false
 
-  def run(target, host) do
+  def run(target, host) when is_env(target) and is_host(host) do
     edeliver_target = target |> Config.edeliver_target()
     edeliver(:deploy, [:release, :to, edeliver_target, "--host=#{host}"])
     restart_host(target, host)
   end
 
-  defp restart_host(env, host) do
+  defp restart_host(env, host) when is_env(env) and is_host(host) do
     controller = Config.controller()
     id = env |> Config.host_id(host)
 
