5b729ccad2df71354bed46af14fd7f599c486b20
Victor Rodrigues <victorc.rodrigues@gmail.com>
2019-03-01 00:38:56 +0100

    Add deli.status test with explicit target

--- test/lib/mix/tasks/deli_status_test.exs
+++ test/lib/mix/tasks/deli_status_test.exs
@@ -47,4 +47,51 @@ defmodule Mix.DeliStatusTest do
       assert output == "# hosts\n## #{hosts |> Enum.join("\n## ")}\n#{log}"
     end
   end
+
+  property "tells if application is running in all target hosts" do
+    check all app <- app(),
+              app_user <- app_user(),
+              env <- env(),
+              hosts <- hosts(),
+              status <- nonempty_string(),
+              short? <- boolean(),
+              running? <- boolean(),
+              verbose? <- boolean() do
+      flag = if short?, do: "-t", else: "--target"
+      put_config(:app, app)
+      put_config(:app_user, [{env, app_user}])
+      put_config(:verbose, verbose?)
+
+      HostProviderMock
+      |> stub(:hosts, fn ^env -> hosts end)
+
+      ControllerMock
+      |> stub(:service_running?, fn ^env, _ -> running? end)
+      |> stub(:service_status, fn ^env, _ -> status end)
+
+      output =
+        capture_io(fn ->
+          :ok = [flag, env] |> Status.run()
+        end)
+
+      log =
+        hosts
+        |> Enum.map(fn host ->
+          id = "#{app_user}@#{host}"
+
+          if running? do
+            if verbose? do
+              "\e[32mrunning #{id}\e[0m\n#{status}\n"
+            else
+              "\e[32mrunning #{id}\e[0m\n"
+            end
+          else
+            "\e[31mnot running #{id}\e[0m\n#{status}\n"
+          end
+        end)
+        |> Enum.join("")
+
+      assert output == "# hosts\n## #{hosts |> Enum.join("\n## ")}\n#{log}"
+    end
+  end
 end
