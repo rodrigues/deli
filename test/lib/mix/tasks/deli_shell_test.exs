8ab539106d0ec35f2af3a52de2c98ddfe9d67a0f
Victor Rodrigues <victorc.rodrigues@gmail.com>
2019-03-09 15:03:14 +0100

    Refactor tests

--- test/lib/mix/tasks/deli_shell_test.exs
+++ test/lib/mix/tasks/deli_shell_test.exs
@@ -7,6 +7,53 @@ defmodule Mix.DeliShellTest do
     put_config(:__system_handler__, SystemStub)
   end
 
+  def setup_shell_test(%{
+        app: app,
+        app_user: app_user,
+        cookie: cookie,
+        bin_path: bin_path,
+        epmd_base_path: epmd_base_path,
+        epmd_port: epmd_port,
+        app_port: app_port,
+        env: env,
+        host: host,
+        whoami: whoami
+      }) do
+    epmd_path = "#{epmd_base_path}/epmd"
+    put_config(:app, app)
+    put_config(:cookie, cookie)
+    put_config(:app_user, [{env, app_user}])
+    put_config(:bin_path, bin_path)
+    put_config(:default_target, env)
+
+    HostFilterMock
+    |> stub(:host, fn ^env, _ -> {:ok, host} end)
+
+    id = "#{app_user}@#{host}"
+
+    epmd_fwd = "-L#{epmd_port}:localhost:#{epmd_port}"
+    app_fwd = "-L#{app_port}:localhost:#{app_port}"
+    epmd_names_call = "#{epmd_path} -names"
+
+    TestAgent.set(:cmd, fn
+      "ssh", [^id, "ps ax | grep epmd | grep erts"], _ ->
+        {epmd_path, 0}
+
+      "ssh", [^id, ^epmd_names_call], _ ->
+        {"port #{epmd_port}\nname #{app} at port #{app_port}", 0}
+
+      "ssh", [^id, ^epmd_fwd, ^app_fwd], _ ->
+        send(TestAgent.get(:pid), :ssh_port_forwarded)
+        {"", 0}
+
+      "ps", ["aux"], _ ->
+        {"proc1nono\nproc2nono\n", 0}
+
+      "whoami", [], _ ->
+        {whoami, 0}
+    end)
+  end
+
   property "outputs shell command to connect to target remote node by default" do
     check all app <- app(),
               app_user <- app_user(),
@@ -18,38 +65,18 @@ defmodule Mix.DeliShellTest do
               env <- env(),
               host <- host(),
               whoami <- nonempty_string() do
-      epmd_path = "#{epmd_base_path}/epmd"
-      put_config(:app, app)
-      put_config(:cookie, cookie)
-      put_config(:app_user, [{env, app_user}])
-      put_config(:bin_path, bin_path)
-      put_config(:default_target, env)
-
-      HostFilterMock
-      |> stub(:host, fn ^env, _ -> {:ok, host} end)
-
-      id = "#{app_user}@#{host}"
-      epmd_fwd = "-L#{epmd_port}:localhost:#{epmd_port}"
-      app_fwd = "-L#{app_port}:localhost:#{app_port}"
-      epmd_names_call = "#{epmd_path} -names"
-
-      TestAgent.set(:cmd, fn
-        "ssh", [^id, "ps ax | grep epmd | grep erts"], _ ->
-          {epmd_path, 0}
-
-        "ssh", [^id, ^epmd_names_call], _ ->
-          {"port #{epmd_port}\nname #{app} at port #{app_port}", 0}
-
-        "ssh", [^id, ^epmd_fwd, ^app_fwd], _ ->
-          send(TestAgent.get(:pid), :ssh_port_forwarded)
-          {"", 0}
-
-        "ps", ["aux"], _ ->
-          {"proc1nono\nproc2nono\n", 0}
-
-        "whoami", [], _ ->
-          {whoami, 0}
-      end)
+      setup_shell_test(%{
+        app: app,
+        app_user: app_user,
+        cookie: cookie,
+        bin_path: bin_path,
+        epmd_base_path: epmd_base_path,
+        epmd_port: epmd_port,
+        app_port: app_port,
+        env: env,
+        host: host,
+        whoami: whoami
+      })
 
       output =
         capture_io(fn ->
@@ -77,38 +104,19 @@ defmodule Mix.DeliShellTest do
               whoami <- nonempty_string(),
               short? <- boolean() do
       flag = if short?, do: "-r", else: "--remote"
-      epmd_path = "#{epmd_base_path}/epmd"
-      put_config(:app, app)
-      put_config(:cookie, cookie)
-      put_config(:app_user, [{env, app_user}])
-      put_config(:bin_path, bin_path)
-      put_config(:default_target, env)
 
-      HostFilterMock
-      |> stub(:host, fn ^env, _ -> {:ok, host} end)
-
-      id = "#{app_user}@#{host}"
-      epmd_fwd = "-L#{epmd_port}:localhost:#{epmd_port}"
-      app_fwd = "-L#{app_port}:localhost:#{app_port}"
-      epmd_names_call = "#{epmd_path} -names"
-
-      TestAgent.set(:cmd, fn
-        "ssh", [^id, "ps ax | grep epmd | grep erts"], _ ->
-          {epmd_path, 0}
-
-        "ssh", [^id, ^epmd_names_call], _ ->
-          {"port #{epmd_port}\nname #{app} at port #{app_port}", 0}
-
-        "ssh", [^id, ^epmd_fwd, ^app_fwd], _ ->
-          send(TestAgent.get(:pid), :ssh_port_forwarded)
-          {"", 0}
-
-        "ps", ["aux"], _ ->
-          {"proc1nono\nproc2nono\n", 0}
-
-        "whoami", [], _ ->
-          {whoami, 0}
-      end)
+      setup_shell_test(%{
+        app: app,
+        app_user: app_user,
+        cookie: cookie,
+        bin_path: bin_path,
+        epmd_base_path: epmd_base_path,
+        epmd_port: epmd_port,
+        app_port: app_port,
+        env: env,
+        host: host,
+        whoami: whoami
+      })
 
       output =
         capture_io(fn ->
@@ -123,7 +131,6 @@ defmodule Mix.DeliShellTest do
     end
   end
 
-  @tag :wip
   property "outputs shell command to observe remote node when -o" do
     check all app <- app(),
               app_user <- app_user(),
@@ -137,38 +144,19 @@ defmodule Mix.DeliShellTest do
               whoami <- nonempty_string(),
               short? <- boolean() do
       flag = if short?, do: "-o", else: "--observer"
-      epmd_path = "#{epmd_base_path}/epmd"
-      put_config(:app, app)
-      put_config(:cookie, cookie)
-      put_config(:app_user, [{env, app_user}])
-      put_config(:bin_path, bin_path)
-      put_config(:default_target, env)
 
-      HostFilterMock
-      |> stub(:host, fn ^env, _ -> {:ok, host} end)
-
-      id = "#{app_user}@#{host}"
-      epmd_fwd = "-L#{epmd_port}:localhost:#{epmd_port}"
-      app_fwd = "-L#{app_port}:localhost:#{app_port}"
-      epmd_names_call = "#{epmd_path} -names"
-
-      TestAgent.set(:cmd, fn
-        "ssh", [^id, "ps ax | grep epmd | grep erts"], _ ->
-          {epmd_path, 0}
-
-        "ssh", [^id, ^epmd_names_call], _ ->
-          {"port #{epmd_port}\nname #{app} at port #{app_port}", 0}
-
-        "ssh", [^id, ^epmd_fwd, ^app_fwd], _ ->
-          send(TestAgent.get(:pid), :ssh_port_forwarded)
-          {"", 0}
-
-        "ps", ["aux"], _ ->
-          {"proc1nono\nproc2nono\n", 0}
-
-        "whoami", [], _ ->
-          {whoami, 0}
-      end)
+      setup_shell_test(%{
+        app: app,
+        app_user: app_user,
+        cookie: cookie,
+        bin_path: bin_path,
+        epmd_base_path: epmd_base_path,
+        epmd_port: epmd_port,
+        app_port: app_port,
+        env: env,
+        host: host,
+        whoami: whoami
+      })
 
       output =
         capture_io(fn ->
@@ -196,45 +184,26 @@ defmodule Mix.DeliShellTest do
               whoami <- nonempty_string(),
               short? <- boolean() do
       flag = if short?, do: "-b", else: "--bin"
-      epmd_path = "#{epmd_base_path}/epmd"
-      put_config(:app, app)
-      put_config(:cookie, cookie)
-      put_config(:app_user, [{env, app_user}])
-      put_config(:bin_path, bin_path)
-      put_config(:default_target, env)
 
-      HostFilterMock
-      |> stub(:host, fn ^env, _ -> {:ok, host} end)
-
-      id = "#{app_user}@#{host}"
-      epmd_fwd = "-L#{epmd_port}:localhost:#{epmd_port}"
-      app_fwd = "-L#{app_port}:localhost:#{app_port}"
-      epmd_names_call = "#{epmd_path} -names"
-
-      TestAgent.set(:cmd, fn
-        "ssh", [^id, "ps ax | grep epmd | grep erts"], _ ->
-          {epmd_path, 0}
-
-        "ssh", [^id, ^epmd_names_call], _ ->
-          {"port #{epmd_port}\nname #{app} at port #{app_port}", 0}
-
-        "ssh", [^id, ^epmd_fwd, ^app_fwd], _ ->
-          send(TestAgent.get(:pid), :ssh_port_forwarded)
-          {"", 0}
-
-        "ps", ["aux"], _ ->
-          {"proc1nono\nproc2nono\n", 0}
-
-        "whoami", [], _ ->
-          {whoami, 0}
-      end)
+      setup_shell_test(%{
+        app: app,
+        app_user: app_user,
+        cookie: cookie,
+        bin_path: bin_path,
+        epmd_base_path: epmd_base_path,
+        epmd_port: epmd_port,
+        app_port: app_port,
+        env: env,
+        host: host,
+        whoami: whoami
+      })
 
       output =
         capture_io(fn ->
           :ok = [flag] |> Shell.run()
         end)
 
-      assert output == "ssh #{id} #{bin_path} remote_console"
+      assert output == "ssh #{app_user}@#{host} #{bin_path} remote_console"
       assert_received :ssh_port_forwarded
     end
   end
